0ac19a2431582f3d7934a5e87f0766f85340689b
NikhilT27 <nikhilthakare14@gmail.com>
2020-12-15 08:58:09 +0530

    config: package.json and index.js for deploy in heroku

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var cors = require("cors");
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,6 +25,6 @@ app.post("/postLocation", (req, res) => {
     });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
